051d5b276f100ba1279f0939f3d6b17f910120dc
cryptrunner49 <cryptrunner49@proton.me>
2024-12-28 01:40:41 +0000

    style modifications

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   #get "home/index"
-  root "home#index"
+  #root "home#index"
+  root "friends#index"
   get "home/about"
 end
